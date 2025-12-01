b7c6713e94b44b9c7417356b106e187eaed5e584
Ceris <c0rd1n340@gmail.com>
2025-12-01 23:30:53 +0300

    Completion

---
+++ HW6/src/Box.java
@@ -0,0 +1,9 @@
+class Box {
+    protected double width;
+    protected double height;
+
+    public Box(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+}
