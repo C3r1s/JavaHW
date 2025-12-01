24177efc037624ab02d2ba375c726a767a86af0e
Ceris <c0rd1n340@gmail.com>
2025-12-01 23:31:52 +0300

    Revert "Completion"
    This reverts commit b7c6713e94b44b9c7417356b106e187eaed5e584.

--- HW6/src/Box.java
+++
@@ -1,9 +0,0 @@
-class Box {
-    protected double width;
-    protected double height;
-
-    public Box(double width, double height) {
-        this.width = width;
-        this.height = height;
-    }
-}
